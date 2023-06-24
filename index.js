3f744d1d554be07ad7ce7f337c68b95f85607345
JorgeMioska <quispechilojorge@gmail.com>
2023-06-24 05:37:29 -0500

    hamburger button js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("object");
+const menu = document.querySelector("#menu");
+const bar = document.querySelectorAll(".bar");
+
+menu.addEventListener("click", () => {
+  bar.forEach((bar) => {
+    if (bar.classList.contains("animation__open")) {
+      bar.classList.remove("animation__open");
+      bar.classList.add("animation__close");
+    } else {
+      bar.classList.remove("animation__close");
+      bar.classList.add("animation__open");
+    }
+  });
+});
