b166f746b9608976a4748691a4159d34309ad479
JorgeMioska <quispechilojorge@gmail.com>
2023-06-24 21:05:57 -0500

    bar-1 ready

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const menu = document.querySelector("#menu");
-const bar = document.querySelectorAll(".bar");
 
 menu.addEventListener("click", () => {
-  bar.forEach((bar) => {
-    if (bar.classList.contains("animation__open")) {
-      bar.classList.remove("animation__open");
-      bar.classList.add("animation__close");
-    } else {
-      bar.classList.remove("animation__close");
-      bar.classList.add("animation__open");
-    }
-  });
+  menu.classList.toggle("open")
 });
