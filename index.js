e6d5166628856b970737b577b66b2fc2135c858d
JorgeMioska <quispechilojorge@gmail.com>
2023-07-11 06:58:34 -0500

    day 7 ready

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const searchInput = document.getElementById("day-7__input");
 searchButton.addEventListener("click", () => {
   searchInput.classList.toggle("search-open");
 });
+
+const day7Button = document.getElementById("dy-7__navbar");
+const hiddenMenu = document.getElementById("dy-7__container");
+const frontMenu = document.getElementById("dy-7__front");
+
+day7Button.addEventListener("click", () => {
+  hiddenMenu.classList.toggle("dy-7__hidden-open");
+  frontMenu.classList.toggle("dy-7__front-open");
+});
